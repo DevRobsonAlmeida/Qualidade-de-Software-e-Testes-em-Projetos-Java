cff2186cbed4f26c51f5c24911a8db6c568b4ecd
Robson Almeida <62033473+DevRobsonAlmeida@users.noreply.github.com>
2024-11-05 21:37:11 -0300

    feat: conhecendo algumas outras assercoes

---
+++ Teste Unitários com JUnit/src/test/java/com/example/demo/entity/AssertionsTeste.java
@@ -0,0 +1,44 @@
+package com.example.demo.entity;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class AssertionsTeste {
+
+    @Test
+    void validarLancamento(){
+        int[] primeirolancamento = {10, 20, 30, 40, 50};
+        int[] segundoLancamento = {10, 2, 3, 4, 50};
+
+        assertNotEquals(primeirolancamento, segundoLancamento);
+
+    }
+
+    @Test
+    void validarObjetoIsNull() {
+        Pessoa pessoa = null;
+
+        assertNull(pessoa);
+
+        pessoa = new Pessoa("Luciano", LocalDateTime.now());
+
+        assertNotNull(pessoa);
+
+    }
+
+
+    @Test
+    void validarObjetoDiferente() {
+        double valor = 5.0;
+        double outroValor = 5.0;
+
+        assertEquals(valor, outroValor);
+
+    }
+
+}
